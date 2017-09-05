fd8bc6c772fb68c58bf6b1543bababd8afef2d8a
Rodrigo <rodrigo.villarroel.m@gmail.com>
2017-09-05 19:30:13 -0400

    new class

---
+++ app/src/main/java/cardGame/Game.java
@@ -0,0 +1,23 @@
+package cardGame;
+
+import cardGame.cards.Meme;
+
+/**
+ * Created by Rodrigo on 5/9/2017.
+ */
+
+public class Game {
+    private MemeStoneUI memeUI;
+
+    public Game(MemeStoneUI memeUI) {
+        this.memeUI = memeUI;
+    }
+
+    public void freeze(Meme meme) {
+
+    }
+
+    public void burn(Meme meme) {
+
+    }
+}
