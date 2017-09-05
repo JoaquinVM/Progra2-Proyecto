d091056defa930cbed7a4fd1e65c701272d6c9e6
Joaco99 <viscafe38@gmail.com>
2017-09-05 17:10:40 -0400

    Commit

---
+++ app/src/main/java/cardGame/utils/Enums.java
@@ -0,0 +1,14 @@
+package cardGame.utils;
+
+/**
+ * Created by Joaco99 on 05/09/2017.
+ */
+
+public class Enums {
+    public enum Tipo{
+        FUEGO,
+        HIELO,
+        AIRE,
+        TIERRA
+    }
+}
