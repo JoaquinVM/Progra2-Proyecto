076124ed348867617bf9384aa625e49279dcabc0
Joaco99 <viscafe38@gmail.com>
2017-09-07 08:46:46 -0400

    deleted things

--- app/src/main/java/cardGame/Player.java
+++ app/src/main/java/cardGame/Player.java
@@ -11,25 +11,13 @@ import cardGame.cards.Meme;
 public abstract class Player implements Damagable{
     private int health;
     private String name;
-    //TODO why String type is needed?
-    private String type;
-    //TODO wht game is needed?
-    private Game game;
-
 
     private boolean dead = false;
     public Player(String type, int health){
-        this.type=type;
         this.health=health;
     }
     public int getHealth(){
         return health;
-    }
-    public String getType(){
-        return type;
-    }
-    public Game getGame(){
-        return game;
     }
     public abstract void power(Meme meme);
 
